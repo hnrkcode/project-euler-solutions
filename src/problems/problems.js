80620feab1afb974eec08980fcb09d5b020f60bc
Henrik Petersson <henrik@tutamail.com>
2020-07-04 18:15:10 +0200

    Add solution to problem 3

--- src/problems/problems.js
+++ src/problems/problems.js
@@ -23,4 +23,17 @@ export default {
 
     return result;
   },
+  problem3(arg = 600851475143) {
+    const primeFactors = [];
+    const multiply = (accumulator, currentValue) => accumulator * currentValue;
+
+    for (let i = 2; ; i++) {
+      if (arg % i === 0) primeFactors.push(i);
+      // Breaks loop when all prime factors are found.
+      if (primeFactors.reduce(multiply, 1) === arg) break;
+    }
+
+    // Return largest prime factor
+    return Math.max(...primeFactors);
+  },
 };
