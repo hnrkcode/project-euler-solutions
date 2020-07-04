621d5f9c249685dd8feb3de949101ae9290187cb
Henrik Petersson <henrik@tutamail.com>
2020-07-04 14:57:21 +0200

    Add solution to problem 1

--- src/problems/problems.js
+++ src/problems/problems.js
@@ -1,6 +1,10 @@
 export default {
-  problem1() {
-    return 1;
+  problem1(arg=1000) {
+    return [...Array(arg).keys()].reduce((accumulator, currentValue) => {
+      if (currentValue % 3 === 0 || currentValue % 5 === 0)
+        return accumulator + currentValue;
+      return accumulator;
+    });
   },
   problem2() {
     return 2;
