4d03c188e49e68dcf0a167f0eef4170bd1d0ce4e
Henrik Petersson <henrik@tutamail.com>
2020-07-05 15:54:37 +0200

    Add solution to problem 7

--- src/problems/problems.js
+++ src/problems/problems.js
@@ -93,4 +93,28 @@ export default {
     // Return the difference between the values.
     return value2 - value1;
   },
+  problem7(arg = 10001) {
+    // Algorithm that checks if it's a prime number.
+    const isPrime = (n) => {
+      if (n <= 3) return n > 1;
+      else if (n % 2 === 0 || n % 3 === 0) return false;
+
+      let i = 5;
+
+      while (i * i <= n) {
+        if (n % i === 0 || n % (i + 2) === 0) return false;
+        i += 6;
+      }
+
+      return true;
+    };
+
+    // Finds the Nth prime number.
+    for (let i = 0, primeCounter = 1; primeCounter <= arg; i++) {
+      if (isPrime(i)) {
+        if (primeCounter === arg) return i;
+        primeCounter++;
+      }
+    }
+  },
 };
