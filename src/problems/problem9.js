76fdb4d8dfdebc12fe223f5c2a11508a1a3531ac
Henrik Petersson <henrik@tutamail.com>
2020-07-08 20:16:07 +0200

    Add solution to problem 9

---
+++ src/problems/problem9.js
@@ -0,0 +1,9 @@
+export const problem9 = (arg = 1000) => {
+  for (let a = 1; a < arg / 3; a++) {
+    for (let b = a, c = 0; b < arg / 2; b++) {
+      c = arg - a - b;
+      // Found the Pythagorean triplet where a + b + c is same as "arg".
+      if (a ** 2 + b ** 2 === c ** 2) return a * b * c;
+    }
+  }
+};
