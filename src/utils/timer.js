f38dd4779b34c8a9f996753bc23dbf8bac8e80c3
Henrik Petersson <henrik@tutamail.com>
2020-07-09 19:42:31 +0200

    Add font awesome icons to runtime rating

--- src/utils/timer.js
+++ src/utils/timer.js
@@ -10,24 +10,34 @@ export const timeFunc = (func) => {
 // Return result and if the execution time is good, ok, or bad.
 export const formatResult = (func) => {
   const [result, time] = timeFunc(func);
+
   const solutionPara = document.createElement("p");
   solutionPara.textContent = `Solution: ${result}`;
   const runtimePara = document.createElement("p");
   runtimePara.textContent = `Runtime: ${time} ms`;
   const ratingPara = document.createElement("p");
   ratingPara.classList.add("has-text-weight-bold");
+  const ratingSpan = document.createElement("span");
+  ratingSpan.classList.add("icon");
+  const ratingIcon = document.createElement("i");
+  ratingSpan.append(ratingIcon);
+  ratingPara.append(ratingSpan);
+
+  const addMessage = (msg) => ratingPara.prepend(document.createTextNode(msg));
 
   if (time > 500 && time < 5000) {
-    ratingPara.textContent = "OK";
+    addMessage("OK");
+    ratingIcon.classList.add("fas", "fa-exclamation-triangle");
     ratingPara.classList.add("has-text-warning");
-    return [solutionPara, runtimePara, ratingPara];
   } else if (time > 5000) {
-    ratingPara.textContent = "BAD";
+    addMessage("BAD");
+    ratingIcon.classList.add("fas", "fa-thumbs-down");
     ratingPara.classList.add("has-text-danger");
-    return [solutionPara, runtimePara, ratingPara];
   } else {
-    ratingPara.textContent = "GOOD";
+    addMessage("GOOD");
+    ratingIcon.classList.add("fas", "fa-thumbs-up");
     ratingPara.classList.add("has-text-success");
-    return [solutionPara, runtimePara, ratingPara];
   }
+
+  return [solutionPara, runtimePara, ratingPara];
 };
